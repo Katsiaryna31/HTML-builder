6a883eebde926ebb67586d8d9048214ed0afdbfb
Katsiaryna31 <katia310790@mail.ru>
2021-11-03 12:36:14 +0300

    feat: add 3-5 tasks

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const {copyFile }= require('fs/promises');
+const  { mkdir } = require ('fs/promises');
+const  { readdir } = require ('fs/promises');
 
+let dir = path.join(__dirname, 'files-copy');
+let currentDir = path.join(__dirname, '/files');
+
+async function copyDirectory () {
+    await mkdir(dir, { recursive: true });
+    try {
+        const files = await readdir(currentDir);
+        for (const file of files) {
+            const currentFile = path.join(__dirname, 'files', file);
+            const destinationFile = path.join(__dirname, 'files-copy', file);
+            fs.WriteStream(destinationFile);
+            await copyFile(currentFile, destinationFile);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+copyDirectory();
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,35 @@
+const  { readdir } = require ('fs/promises');
+const fs = require('fs');
+const path = require('path');
+
+let stylesDir = path.join(__dirname, 'styles');
+let destinationFile = path.join(__dirname, 'project-dist', 'bundle.css');
+
+async function readDirectory() {
+    fs.WriteStream(destinationFile);
+    try {
+        const files = await readdir(stylesDir);
+        for (const file of files) {
+            const fileJoin = path.join(__dirname, 'styles', file);
+            fs.stat(fileJoin, (error, stats) => {
+                if (error) {
+                  console.log(error);
+                }
+                else {
+                  if (!stats.isDirectory() && path.extname(file) === '.css') {
+                    let readableStream = fs.createReadStream(fileJoin,'utf8')
+                    readableStream.on('data', function (data) {
+                        fs.appendFile(destinationFile, data, (err) => {
+                            if (err) throw err;
+                        });
+                    }) 
+                  }
+                }
+            }) 
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+readDirectory()
