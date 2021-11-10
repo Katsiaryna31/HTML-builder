2c42d9199aae25acb1489a3969df5a959896296b
Katsiaryna31 <katia310790@mail.ru>
2021-11-10 23:01:05 +0300

    fix: add deleting directory from 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const {copyFile }= require('fs/promises');
-const  { mkdir } = require ('fs/promises');
+const  { mkdir, rmdir } = require ('fs/promises');
 const  { readdir } = require ('fs/promises');
 
 let dir = path.join(__dirname, 'files-copy');
-let currentDir = path.join(__dirname, '/files');
+let currentDir = path.join(__dirname, 'files');
 
 async function copyDirectory () {
+    await rmdir(dir, { recursive: true });
     await mkdir(dir, { recursive: true });
     try {
         const files = await readdir(currentDir);
